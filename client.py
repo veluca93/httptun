9471c2809b7ddc355b8cb19463f66c5150044f78
Luca Versari <veluca93@gmail.com>
2018-03-14 22:11:52 +0100

    Use connection pooling

--- client.py
+++ client.py
@@ -10,12 +10,13 @@ BROADCAST = b'\xff\xff\xff\xff\xff\xff'
 
 
 def read_data():
+    session = requests.Session()
     while True:
         wdata = tap.read(tap.mtu)
         # Bytes 0-1 are "flags", bytes 2-3 are a copy of the protocol.
         # From byte 4 on is the real ethernet frame.
         #wdata = wdata[4:]
-        wans = requests.post(server + '/send', my_mac + wdata)
+        wans = session.post(server + '/send', my_mac + wdata)
         if wans.status_code != 200:
             print("send: received status code " + str(wans.status_code) +
                   ": " + wans.text)
@@ -27,9 +28,10 @@ if __name__ == '__main__':
     while server.endswith('/'):
         server = server[:-1]
 
-    ans = requests.post(server + '/connect', 'very_secret', stream=True).raw
-    my_mac = ans.read(6)
-    my_ip = ans.read(4)
+    session = requests.Session()
+    ans = session.post(server + '/connect', 'very_secret').content
+    my_mac = ans[:6]
+    my_ip = ans[6:10]
     tap = TunTapDevice(flags=IFF_TAP)
     tap.addr = ".".join(map(str, my_ip))
     tap.netmask = '255.255.0.0'
@@ -40,7 +42,7 @@ if __name__ == '__main__':
     tap_reader.start()
 
     while True:
-        ans = requests.post(server + '/recv', my_mac, stream=True)
+        ans = session.post(server + '/recv', my_mac)
         if ans.status_code == 204:
             time.sleep(0.1)
             continue
@@ -48,6 +50,6 @@ if __name__ == '__main__':
             print("recv: received status code " + ans.status_code + ": " +
                   ans.text)
             sys.exit(1)
-        data = ans.raw.read()
+        data = ans.content
         if data[4:10] == my_mac or data[4:10] == BROADCAST:
             tap.write(data)
