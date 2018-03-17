226c78a4cf27579f840291ce5231f9658738d014
Luca Versari <veluca93@gmail.com>
2018-03-17 16:04:16 +0100

    Add support for multiple packets in the same http request

--- common.py
+++ common.py
@@ -1,4 +1,8 @@
+from queue import Empty
+import struct
+
 BROADCAST = b'\xff\xff\xff\xff\xff\xff'
+HDR_FORMAT = '!i'
 
 
 def get_mac(data):
@@ -9,12 +13,23 @@ def get_mac(data):
 
 
 def dequeue(queue, timeout=None):
-    return [queue.get(timeout=timeout)]
+    result = [queue.get(timeout=timeout)]
+    try:
+        while True:
+            result.append(queue.get(False))
+    except Empty:
+        return result
 
 
 def parse_packets(stream, callback):
-    callback(stream.read())
+    while True:
+        hdr = stream.read(struct.calcsize(HDR_FORMAT))
+        if not hdr:
+            return
+        data = stream.read(struct.unpack(HDR_FORMAT, hdr))
+        callback(data)
 
 
 def serialize_packets(packets):
-    return packets[0]
+    return b"".join(
+        struct.pack(HDR_FORMAT, len(packet)) + packet for packet in packets)
